f25d8a317f7bd1f44d1290fc21c016f4daaba996
hak9ya <63631952+hak9ya@users.noreply.github.com>
2020-10-01 18:14:35 +0900

    Add files via upload

---
+++ Scripts/MonsterMove.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterMove : MonoBehaviour
+{
+    Rigidbody2D rigid;
+    Animator anim;
+    SpriteRenderer spriteRenderer;
+    CapsuleCollider2D capsuleCollider;
+    public int nextMove;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        capsuleCollider = GetComponent<CapsuleCollider2D>();
+
+        Invoke("Think", 2); // 주어진 시간이 지난 뒤, 지정된 함수를 실행
+    }
+
+    void FixedUpdate()
+    {
+        // 몬스터 움직임
+        rigid.velocity = new Vector2(nextMove, rigid.velocity.y);
+
+        // 오브젝트 체크
+        Vector2 frontVec = new Vector2(rigid.position.x + nextMove * 0.3f, rigid.position.y);
+        Debug.DrawRay(frontVec, Vector3.down, new Color(0, 1, 0));
+        // RayCastHit = Ray에 닿은 오브젝트
+        RaycastHit2D rayHit = Physics2D.Raycast(frontVec, Vector3.down, 1, LayerMask.GetMask("Platform"));
+
+        if (rayHit.collider == null)
+            Turn();
+
+    }
+
+    //재귀 함수
+    void Think()
+    {
+        nextMove = Random.Range(-1, 2); // 랜덤으로 행동하게 만듬, AI 필수
+
+        float nextThinkTime = Random.Range(2f, 5f);
+        Invoke("Think", 5);
+
+        // 애니메이션 변경
+        anim.SetInteger("WalkSpeed", nextMove);
+        
+        // 방향전환
+        if (nextMove != 0)
+            spriteRenderer.flipX = nextMove == 1;
+
+    }
+
+    void Turn()
+    {
+        nextMove = nextMove * -1;
+        spriteRenderer.flipX = nextMove == 1;
+        CancelInvoke();
+        Invoke("Think", 2);
+    }
+
+    public void OnDamaged()
+    {
+        // 투명도
+        spriteRenderer.color = new Color(1, 1, 1, 0.4f);
+
+        // Y축 뒤집기
+        spriteRenderer.flipY = true;
+
+        // 충돌 이벤트 끔
+        capsuleCollider.enabled = false;
+
+        // 밟혔을 때 물리작용
+        rigid.AddForce(Vector2.up * 5, ForceMode2D.Impulse);
+
+        // 시간차 두고 사라짐
+        Invoke("DeActive", 2);
+    }
+
+    void DeActive()
+    {
+        gameObject.SetActive(false);
+    }
+}
---
+++ Scripts/PlayerMove.cs
@@ -0,0 +1,221 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerMove : MonoBehaviour
+{
+    public GameManager gameManager;
+    public AudioClip audioJump;
+    public AudioClip audioAttack;
+    public AudioClip audioDamaged;
+    public AudioClip audioItem;
+    public AudioClip audioDie;
+    public AudioClip audioFinish;
+    public float maxSpeed;
+    public float jumpPower;
+   
+    Rigidbody2D rigid;
+    SpriteRenderer spriteRenderer;
+    CapsuleCollider2D capsuleCollider;
+    Animator anim;
+    AudioSource audioSource;
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        capsuleCollider = GetComponent<CapsuleCollider2D>();
+        anim = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+    }
+    void PlaySound(string action)
+    {
+        switch (action)
+        {
+            case "JUMP":
+                audioSource.clip = audioJump;
+                break;
+            case "ATTACK":
+                audioSource.clip = audioAttack;
+                break;
+            case "DAMAGED":
+                audioSource.clip = audioDamaged;
+                break;
+            case "ITEM":
+                audioSource.clip = audioItem;
+                break;
+            case "DIE":
+                audioSource.clip = audioDie;
+                break;
+            case "FINISH":
+                audioSource.clip = audioFinish;
+                break;
+        }
+    }
+
+
+        void Update()
+    {
+
+        // 점프
+        if (Input.GetButtonDown("Jump") && !anim.GetBool("isJumping"))
+        {
+            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            anim.SetBool("isJumping", true);
+            PlaySound("JUMP");
+            audioSource.Play();
+        }
+
+        // 정지 속도
+        if (Input.GetButtonUp("Horizontal"))
+        {
+            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
+                                             // normalized = 방향 구할때(벡터 크기를 1로 만든 상태)
+        }
+
+        // 방향 전환
+        if (Input.GetButton("Horizontal"))
+            spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
+
+       
+        // 애니메이션
+        if (Mathf.Abs(rigid.velocity.x) < 0.3)
+            anim.SetBool("isWalking", false);
+        else
+            anim.SetBool("isWalking", true);
+    }
+    void FixedUpdate()
+    {
+        //Move By Key Control
+        float h = Input.GetAxisRaw("Horizontal");
+
+        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+
+        if (rigid.velocity.x > maxSpeed) // right speed
+           // velocity = rigidBody의 현재 속도
+            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
+
+        else if(rigid.velocity.x < maxSpeed * (-1)) // left speed
+            rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);
+
+        // RayCast = 오브젝트 검색을 위해 Ray를 쏘는 방식
+       if(rigid.velocity.y < 0)
+        {
+            Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0));
+
+            // RayCastHit = Ray에 닿은 오브젝트
+            RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
+
+            if (rayHit.collider != null)
+            {
+                if (rayHit.distance < 0.5f)
+                    anim.SetBool("isJumping", false);
+
+            }
+
+        }
+        // LayerMask = 물리 효과를 구분하는 정수값
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Monster")
+        {
+            // 밟아 죽이기
+            if (rigid.velocity.y < 0 && transform.position.y > collision.transform.position.y)
+            {
+                OnAttack(collision.transform);
+            }
+            else
+                OnDamaged(collision.transform.position);
+            
+        }   
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Item")
+        {
+            bool isBronze = collision.gameObject.name.Contains("Bronze"); // contains(비교문) : 대상 문자열에 비교문이 있으면 true
+            bool isSilver = collision.gameObject.name.Contains("Silver");
+            bool isGold = collision.gameObject.name.Contains("Gold");
+
+            if (isBronze)
+                gameManager.stagePoint += 50;
+            else if (isSilver)
+                gameManager.stagePoint += 100;
+            else if (isGold)
+                gameManager.stagePoint += 300;
+
+            collision.gameObject.SetActive(false);
+
+            PlaySound("ITEM");
+            audioSource.Play();
+        }
+        else if (collision.gameObject.tag == "Finish")
+        {
+            gameManager.NextStage();
+            PlaySound("FINISH");
+            audioSource.Play();
+        }
+    }
+
+    void OnAttack(Transform monster)
+    {
+        gameManager.stagePoint += 100;
+
+        rigid.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+
+        MonsterMove monsterMove = monster.GetComponent<MonsterMove>();
+        monsterMove.OnDamaged();
+        PlaySound("ATTACK");
+        audioSource.Play();
+    }
+
+    void OnDamaged(Vector2 targetPos)
+    {
+        gameManager.HealthDown();
+
+        gameObject.layer = 11;
+
+        spriteRenderer.color = new Color(1, 1, 1, 0.4f);
+        int dirc = transform.position.x - targetPos.x > 0 ? 1 : -1;
+        rigid.AddForce(new Vector2(dirc, 1) * 7, ForceMode2D.Impulse);
+
+
+        anim.SetTrigger("doDamaged");
+        Invoke("OffDamaged", 3);
+        PlaySound("DAMAGED");
+        audioSource.Play();
+    }
+
+    void OffDamaged()
+    {
+        gameObject.layer = 10;
+        spriteRenderer.color = new Color(1, 1, 1, 1);
+    }
+
+    public void OnDie()
+    {
+        // 투명도
+        spriteRenderer.color = new Color(1, 1, 1, 0.4f);
+
+        // Y축 뒤집기
+        spriteRenderer.flipY = true;
+
+        // 충돌 이벤트 끔
+        capsuleCollider.enabled = false;
+
+        // 밟혔을 때 물리작용
+        rigid.AddForce(Vector2.up * 5, ForceMode2D.Impulse);
+
+        PlaySound("DIE");
+        audioSource.Play();
+
+    }
+
+    public void VelocityZero()
+    {
+        rigid.velocity = Vector2.zero;
+    }
+}
